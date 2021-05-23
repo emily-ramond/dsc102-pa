5606e36bc6363d37a9a94bf8c3f2fc196e671baf
Emily Ramond <72273345+emily-ramond@users.noreply.github.com>
2021-05-22 22:22:09 -0700

    Update ec2_initialization.sh

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-sudo apt install python3-pip
+sudo apt update
+sudo apt install python3-pip -y
 pip install "dask[complete]" --no-cache-dir
-sudo apt install awscli
+sudo apt install awscli -y
 export AWS_ACCESS_KEY_ID=ASIAXQPRUKICJDXUQ6ZU
 export AWS_SECRET_ACCESS_KEY=/ooErsu9hff1Aio/fHJO/kY8xw0iqNsejqVvpBjh
 export AWS_SESSION_TOKEN=FwoGZXIvYXdzECoaDOh2hQuwBtDnHCeZOCKsAfx9DOuZsh8Q9rrzPK/k6TMhB5+APWCQKKmN1XKW++o3ykGOlsWgLK6zu2Pv7qO0Sz2AJ89hQuSXOsZs2FlclMU3zHPNgqJ8YB3N5qkORY+69LIN480zR72i4NJQtQ6DkshUr3tDmWgLEEY22jhEikEUUf3d/KctDaXlxJt160fOPfNtM3AZIMwI2Wr2zFFvQnrBMKFK2S9Pla9djiwNTCGUzdJSFRYNaoDigREo6KuhhQYyLdnJXkB/ESVslC7NNiGdRfMo5rNn+emku9Fb6JSiZZdxqYwhP9SQXEgVNLnEZg==
