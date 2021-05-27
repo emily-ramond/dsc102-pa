76c7cfe57722341860747ec0fc2c5838211abf79
Emily Ramond <72273345+emily-ramond@users.noreply.github.com>
2021-05-26 22:56:02 -0700

    Update ec2_initialization.sh

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -3,6 +3,7 @@ sudo apt update
 sudo apt install python3-pip -y
 pip install "dask[complete]" --no-cache-dir
 sudo apt install awscli -y
+pip install dask-ml
 export AWS_ACCESS_KEY_ID=ASIAXQPRUKICLKTG645R
 export AWS_SECRET_ACCESS_KEY=wimYAUICs6zt3fEwsHvfpFZuZjFnWUOPFSDATrg4
 export AWS_SESSION_TOKEN=FwoGZXIvYXdzEKb//////////wEaDEzpjMKKGd/jLFDbGyKsAc3guMgvZOHtISZNzh+e9ffZHSRMKP1yQh3YMAHWcDBOIXutueyf/ofA6bOQXEw3DAyeJjEKjHSBmOuw/Byn8SXUbAM5vO/37TmpuQpGtJJt/PWrBjuOMGNl1KAkl6xYcr4iP4HTBJya/BTu0PiFdphjEeyOoMPwjU5oDDIIf8K+OjHDWhMy11OUHnrUrzPKs2OAwQJGhdVCMKTHvzZSvAEczRy1zB6grBbv9n8ojMm8hQYyLennTVywFk6lre234T1wB1hXwYcQTFH54m5EPu1pmtkhCMFlqQCngxJ96St1HQ==
