e51053b72a2d854fe252ef516c33c0d2577a7a14
Emily Ramond <72273345+emily-ramond@users.noreply.github.com>
2021-05-28 19:30:42 -0700

    Update ec2_initialization.sh

--- ec2_initialization.sh
+++ ec2_initialization.sh
@@ -4,9 +4,9 @@ sudo apt install python3-pip -y
 pip install "dask[complete]" --no-cache-dir
 sudo apt install awscli -y
 pip install dask-ml
-export AWS_ACCESS_KEY_ID=ASIAXQPRUKICCEBM37LY
-export AWS_SECRET_ACCESS_KEY=h7MaeoGmA1cQXkUo63AnnmKJOovqr23yUXRoByNQ
-export AWS_SESSION_TOKEN=FwoGZXIvYXdzEMv//////////wEaDEyWzsYjtQsefZy/MiKsAVSP/DTIqJ7AheLwiMKW0eoKmlRlJHVavpQUQfOcQMzxfjzJ24JaWboPR4qq4Q+hyBwMsGV/+psbEUs+BZQWQTgqhVnRaFOX5dpmYgzCDl0RrWSM0vnfDcCAl9Ok3UrRDR+3Zp+iMu98SQ3jZFFm9kznmGtTyelFMhNlDqJ9bc32ESpcfxrHdNDQfvr9n24vgvHQTf0iVzvRZ9QuiFMA4siaKDhSO8XlgoUtaaUondXEhQYyLbLLkieBo/cYfqrqdwo5xrCgMBbCdt0f4N7unizA6J439rnuZoHPUEjCT6NNow==
+export AWS_ACCESS_KEY_ID=ASIAXQPRUKICJWD2SPEH
+export AWS_SECRET_ACCESS_KEY=z9OZdSvuJCbsjYsHwuvacrwl+VSHxDHvIHnc0IBb
+export AWS_SESSION_TOKEN=FwoGZXIvYXdzENT//////////wEaDE8mu2EYF4Y0+gDzmiKsAdU4AlYeXdKCyjMBH/z5KVQP3SgAskWsGuwsYsRI9XZj+EwqJyBo370ECozbViUkNld2EgFELwysiAplq9NguDeh3Ph8JpPq/v0J6nme7ipWB1VAyRl4vSau6mpY0MvXhUk+8ctyS48+g5u8Nt1nB78REpnkx3kQlXJLuYypYeiKotwvmIueFhF7CMzj71/WFsf/o+bcxFZfbIHaADYMWtp4Mx/vHMfR7kq8fJkovc7GhQYyLY83mcPqo8Ow8P5jX8Q+TkPMKnYo2VcK8wjjGvNuVNjQiQsFBog/5tfaozJ96g==
 aws s3 cp s3://ds102-maxandemily-scratch/historical_data_2009Q1.txt historical_data_2009Q1.txt
 aws s3 cp s3://ds102-maxandemily-scratch/historical_data_time_2009Q1.txt historical_data_time_2009Q1.txt
 aws s3 cp s3://ds102-maxandemily-scratch/feature_prep.py feature_prep.py
